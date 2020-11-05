b4ec42e214a84b7bcde9a8d2f1d4f207b3668207
Behzad Shomali <behzad.shomali@gmail.com>
2020-11-05 23:11:01 +0330

    Completeing methods: mine, recieve, gossip and so on

--- Java/src/basic_blockchain/Basic_Blockchain.java
+++ Java/src/basic_blockchain/Basic_Blockchain.java
@@ -1,14 +1,18 @@
 package basic_blockchain;
 
 import java.util.Scanner;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Basic_Blockchain {
-    void mine(){
-        
-    }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+   
         Scanner scan = new Scanner(System.in);
+        System.out.print("Please enter the initial number of nodes: ");
         int nodes_number = scan.nextInt();
         for(int id = 0; id < nodes_number; id++){
             Node node = new Node(id);
@@ -17,9 +21,17 @@ public class Basic_Blockchain {
         for (Node node : Parameters.nodes) {
             for (int i = 0; i < node.costs.size(); i++)
                 System.out.println("NodeID: " + node.id + " " + i + " " + node.costs.get(i));
-                
         }
         
-    }
-    
+        int period = (int)Math.ceil(Parameters.period_calculator());
+        Runnable mine_runnable = () -> {
+            try {
+                Parameters.mine();
+            } catch (Exception ex) {
+                Logger.getLogger(Basic_Blockchain.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        };
+        ScheduledExecutorService exec = Executors.newScheduledThreadPool(1);
+        exec.scheduleAtFixedRate(mine_runnable, 0, period, TimeUnit.SECONDS);
+    }   
 }
--- Java/src/basic_blockchain/Parameters.java
+++ Java/src/basic_blockchain/Parameters.java
@@ -1,38 +1,26 @@
 package basic_blockchain;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class Parameters {
-    public static int edge_value_range = 50;
-    public static double prob = 0.5; // The probability that there is no edge between two nodes
-    public static double pace = 0.5;
-    public static LinkedList<Node> nodes = new LinkedList();
-    
-    
-    public static String randomGenerate(){
-        int leftLimit = 48;
-        int rightLimit = 122;
-        int targetStringLength = 32;
-        Random random = new Random();
-        StringBuilder buffer = new StringBuilder(targetStringLength);
-        for (int i = 0; i < targetStringLength; i++) {
-            int randomLimitedInt = leftLimit + (int) 
-              (random.nextFloat() * (rightLimit - leftLimit + 1));
-            buffer.append((char) randomLimitedInt);
-        }
-        return buffer.toString();
-    }
+    public static int edge_value_range = 25;
+    public static double prob = 0.2; // The probability that there is no edge between two nodes
+    public static double pace = 0.5; // The more pace value would be, the faster everything is done
+    public static ArrayList<Node> nodes = new ArrayList<>();
     
     public static double period_calculator(){
         return Parameters.pace * Parameters.nodes.size();
     }
     
-    public static void mine(){
+    public static void mine() throws Exception{
         Random random = new Random();
         int randomIndx = random.nextInt(Parameters.nodes.size()); 
         Node lucky_node = Parameters.nodes.get(randomIndx);
-        System.out.println("Congratulations to node " + lucky_node.id + " for being our next minner");
+        System.out.println("Congratulations to node " + lucky_node.id + " for being our next minner!");
+        String block = RSA.sign(lucky_node.memory_pool, lucky_node.keys.getPrivate());
+        System.out.println("Node " + lucky_node.id + " just signed its memory pool! ");
+        lucky_node.gossip(block);
     }
 
 }
